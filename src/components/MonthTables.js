5a8e694775fa2d96cd5339c587657ec0213e03c6
bijonl <bijon2591@gmail.com>
2023-11-12 09:55:32 -0500

    adding comments

--- src/components/MonthTables.js
+++ src/components/MonthTables.js
@@ -14,21 +14,22 @@ function MonthTables({ monthsToDisplay, allTransactionData, loading }) {
     return points;
   }
 
+  // Function to Return points is date, dataset, and customer ID are provided
   function getPointsPerMonthByID(data, date, ID) {
+    // Filters Data array to transactions that month
     const filteredTransactionsByMonth = data.filter((transaction) => date.toLocaleString('default', { month: 'long', year: 'numeric' }) == new Date(transaction.transactionDate).toLocaleString('default', { month: 'long', year: 'numeric' }));
-
+    // Filters the Data Array for that month to specific Customer ID
     const filteredMonthlyTransactionsByID = filteredTransactionsByMonth.filter((transaction) => transaction.customerID == ID);
-
+    // Map the transaction amounts, converts them to points
     const transactionPointAmountsMonthlyByID = filteredMonthlyTransactionsByID.map((transaction) => {
       const transactionAmtDollars = Math.floor(transaction.transactionAmt.replace(/[^0-9.-]+/g, ''));
       return calculateRewardPoints(transactionAmtDollars);
     });
-
+    // Reduce them down to sum up the values. 
     const totalPoints = transactionPointAmountsMonthlyByID.reduce(
       (accumulator, currentValue) => accumulator + currentValue,
       0,
     );
-
     return totalPoints;
   }
 
@@ -40,17 +41,16 @@ function MonthTables({ monthsToDisplay, allTransactionData, loading }) {
     });
     return customerName;
   }
-
+  // Take all data and get all unique IDs so that there are no repeat rows. 
   const uniqueCustomerIDs = ([...new Set(allTransactionData.map((element) => element.customerID))]);
   const monthHeaders = monthsToDisplay.map((date, i) => (
     <th key={i}>{date.toLocaleString('default', { month: 'long', year: 'numeric' })}</th>
   ));
-
+  // Map Unique Customers and get their data by Month
   const tableDataRows = uniqueCustomerIDs.map((customerID, i) => {
     const customerName = getCustomerNameByID(allTransactionData, customerID);
     const monthlyDataRows = monthsToDisplay.map((date, i) => (
-      <td key={i}>{getPointsPerMonthByID(allTransactionData, date, customerID)}</td>
-
+      <td key={i}>{getPointsPerMonthByID(allTransactionData, date, customerID)}</td> // Creates data rows from above. 
     ));
 
     return (
@@ -62,6 +62,7 @@ function MonthTables({ monthsToDisplay, allTransactionData, loading }) {
       </tr>
     );
   });
+  // Check if it is finished loading before returning. 
   if (loading == false) {
     return (
       <table>
