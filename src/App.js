0f3471832a4ee8d9455aa3174d91f0682b85fcbf
bijonl <bijon2591@gmail.com>
2023-11-12 12:51:08 -0500

    linting

--- src/App.js
+++ src/App.js
@@ -1,39 +1,34 @@
 import MonthTables from './components/MonthTables.js'
-import './styles/Table.css';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import './styles/Table.css'
+import { useState, useEffect } from 'react'
 
+function App () {
+  const API_URL = 'http://localhost:3500/transactions'
+  const [allTransactionData, setAllTransactionData] = useState([])
+  const [monthsToDisplay, setMonthsToDisplay] = useState([])
+  const [numberOfMonthsDisplayed, setnumberOfMonthsDisplayed] = useState(3)
+  const [loading, setLoading] = useState(false)
 
-
-function App() {
-
-  const API_URL = 'http://localhost:3500/transactions';
-  const [allTransactionData, setAllTransactionData] = useState([]); 
-  const [monthsToDisplay, setMonthsToDisplay] = useState([]); 
-  const [numberOfMonthsDisplayed, setnumberOfMonthsDisplayed] = useState(3); 
-  const [loading, setLoading] = useState(false);
-
-  
   useEffect(() => {
     const fetchData = async () => {
-        setLoading(true); 
-        const response = await fetch(API_URL);
-        const newData = await response.json();
-        setAllTransactionData(newData);
-        setLoading(false); // Stop loading
-      }
-      
-      fetchData(); 
-  }, []);
+      setLoading(true)
+      const response = await fetch(API_URL)
+      const newData = await response.json()
+      setAllTransactionData(newData)
+      setLoading(false) // Stop loading
+    }
+
+    fetchData()
+  }, [])
 
   useEffect(() => {
-    // Loops the number of months, gets the date and pushes it into the months to display Array. 
-      for(let month = 0; month < numberOfMonthsDisplayed; month++) {
-      let currentDate = new Date();
-      currentDate.setMonth(currentDate.getMonth() - month);
-      monthsToDisplay.push(currentDate); 
-    }; 
-  }, []);
+    // Loops the number of months, gets the date and pushes it into the months to display Array.
+    for (let month = 0; month < numberOfMonthsDisplayed; month++) {
+      const currentDate = new Date()
+      currentDate.setMonth(currentDate.getMonth() - month)
+      monthsToDisplay.push(currentDate)
+    };
+  }, [])
 
   return (
     <div className="App">
@@ -44,7 +39,7 @@ function App() {
           monthsToDisplay = {monthsToDisplay}
         />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
