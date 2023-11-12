7bc4eeb748eea4c402406b7654463b9299de4da9
bijonl <bijon2591@gmail.com>
2023-11-12 09:44:35 -0500

    lint

--- src/App.js
+++ src/App.js
@@ -1,48 +1,43 @@
-import MonthTables from './components/MonthTables.js'
+import MonthTables from './components/MonthTables.js';
 import './styles/Table.css';
-import { useState } from 'react';
-import { useEffect } from 'react';
-
-
+import { useState, useEffect } from 'react';
 
 function App() {
-
   const API_URL = 'http://localhost:3500/transactions';
-  const [allTransactionData, setAllTransactionData] = useState([]); 
-  const [monthsToDisplay, setMonthsToDisplay] = useState([]); 
-  const [numberOfMonthsDisplayed, setnumberOfMonthsDisplayed] = useState(3); 
+  const [allTransactionData, setAllTransactionData] = useState([]);
+  const [monthsToDisplay, setMonthsToDisplay] = useState([]);
+  const [numberOfMonthsDisplayed, setnumberOfMonthsDisplayed] = useState(3);
   const [loading, setLoading] = useState(false);
 
-  
   useEffect(() => {
     const fetchData = async () => {
-        setLoading(true); 
-        const response = await fetch(API_URL);
-        const newData = await response.json();
-        setAllTransactionData(newData);
-        setLoading(false); // Stop loading
-      }
-      
-      fetchData(); 
+      setLoading(true);
+      const response = await fetch(API_URL);
+      const newData = await response.json();
+      setAllTransactionData(newData);
+      setLoading(false); // Stop loading
+    };
+
+    fetchData();
   }, []);
 
   useEffect(() => {
-    // Loops the number of months, gets the date and pushes it into the months to display Array. 
-      for(let month = 0; month < numberOfMonthsDisplayed; month++) {
-      let currentDate = new Date();
+    // Loops the number of months, gets the date and pushes it into the months to display Array.
+    for (let month = 0; month < numberOfMonthsDisplayed; month++) {
+      const currentDate = new Date();
       currentDate.setMonth(currentDate.getMonth() - month);
-      monthsToDisplay.push(currentDate); 
-    }; 
+      monthsToDisplay.push(currentDate);
+    }
   }, []);
 
   return (
     <div className="App">
-        <h1>Person's Customer Rewards</h1>
-        <MonthTables // Component for each month table
-          loading = {loading}
-          allTransactionData = {allTransactionData}
-          monthsToDisplay = {monthsToDisplay}
-        />
+      <h1>Person's Customer Rewards</h1>
+      <MonthTables // Component for each month table
+        loading={loading}
+        allTransactionData={allTransactionData}
+        monthsToDisplay={monthsToDisplay}
+      />
     </div>
   );
 }
--- src/components/MonthTables.js
+++ src/components/MonthTables.js
@@ -1,99 +1,84 @@
+function MonthTables({ monthsToDisplay, allTransactionData, loading }) {
+  // Loop though the months to create the tables. The array has store the timestamps of the days.
 
-// import TableDataRows from './TableDataRows'; 
-
-function MonthTables({monthsToDisplay, allTransactionData, loading}) {
-    // Loop though the months to create the tables. The array has store the timestamps of the days. 
-
-    function calculateRewardPoints(dollars) {
-        let points = 0; 
-        if(dollars >= 50) {
-          if(dollars - 100 >= 0) {
-            points += ((100 - 50)); 
-            points += ((dollars - 100)*2); 
-          } else {
-            points += (dollars - 50); 
-          }
-        } 
-        return points; 
+  function calculateRewardPoints(dollars) {
+    let points = 0;
+    if (dollars >= 50) {
+      if (dollars - 100 >= 0) {
+        points += ((100 - 50));
+        points += ((dollars - 100) * 2);
+      } else {
+        points += (dollars - 50);
+      }
     }
+    return points;
+  }
 
+  function getPointsPerMonthByID(data, date, ID) {
+    const filteredTransactionsByMonth = data.filter((transaction) => date.toLocaleString('default', { month: 'long', year: 'numeric' }) == new Date(transaction.transactionDate).toLocaleString('default', { month: 'long', year: 'numeric' }));
 
-    
-    function getPointsPerMonthByID(data, date, ID) {
-        const filteredTransactionsByMonth = data.filter((transaction) => { 
-            return date.toLocaleString('default', { month: 'long',   year: "numeric"}) == new Date(transaction.transactionDate).toLocaleString('default', { month: 'long',   year: 'numeric'}); 
-        }); 
+    const filteredMonthlyTransactionsByID = filteredTransactionsByMonth.filter((transaction) => transaction.customerID == ID);
 
-        const filteredMonthlyTransactionsByID = filteredTransactionsByMonth.filter((transaction) => {
-            return transaction.customerID == ID; 
-        }); 
+    const transactionPointAmountsMonthlyByID = filteredMonthlyTransactionsByID.map((transaction) => {
+      const transactionAmtDollars = Math.floor(transaction.transactionAmt.replace(/[^0-9.-]+/g, ''));
+      return calculateRewardPoints(transactionAmtDollars);
+    });
 
-        const transactionPointAmountsMonthlyByID = filteredMonthlyTransactionsByID.map((transaction) => {
-            const transactionAmtDollars = Math.floor(transaction.transactionAmt.replace(/[^0-9.-]+/g, ""))
-            return calculateRewardPoints(transactionAmtDollars); 
-        })
+    const totalPoints = transactionPointAmountsMonthlyByID.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0,
+    );
 
-        const totalPoints = transactionPointAmountsMonthlyByID.reduce(
-            (accumulator, currentValue) => accumulator + currentValue,
-            0,
-          );
+    return totalPoints;
+  }
 
-        return totalPoints;  
-    }
-  
-    function getCustomerNameByID(transactionData, id) {
-        let customerName = ''; 
-        const transactionsWithID = transactionData.filter((transaction) => transaction.customerID == id); 
-        transactionsWithID.forEach((customer) => {
-            customerName = customer.name; 
-        }); 
-        return customerName; 
-    }
+  function getCustomerNameByID(transactionData, id) {
+    let customerName = '';
+    const transactionsWithID = transactionData.filter((transaction) => transaction.customerID == id);
+    transactionsWithID.forEach((customer) => {
+      customerName = customer.name;
+    });
+    return customerName;
+  }
 
-    const uniqueCustomerIDs = ([...new Set(allTransactionData.map((element) => element.customerID))]); 
-    const monthHeaders = monthsToDisplay.map((date, i) => {
-        return (
-            <th key={i}>{date.toLocaleString('default', { month: 'long',   year: "numeric"})}</th>
-        )
-    });  
+  const uniqueCustomerIDs = ([...new Set(allTransactionData.map((element) => element.customerID))]);
+  const monthHeaders = monthsToDisplay.map((date, i) => (
+    <th key={i}>{date.toLocaleString('default', { month: 'long', year: 'numeric' })}</th>
+  ));
 
-    const tableDataRows = uniqueCustomerIDs.map((customerID, i) => {
-        const customerName = getCustomerNameByID(allTransactionData, customerID)
-        const monthlyDataRows = monthsToDisplay.map((date, i) => {
-        return (
-            <td key={i}>{getPointsPerMonthByID(allTransactionData, date, customerID)}</td>
-            
-        )
-    }); 
+  const tableDataRows = uniqueCustomerIDs.map((customerID, i) => {
+    const customerName = getCustomerNameByID(allTransactionData, customerID);
+    const monthlyDataRows = monthsToDisplay.map((date, i) => (
+      <td key={i}>{getPointsPerMonthByID(allTransactionData, date, customerID)}</td>
 
-        return (
-            <tr>
-                <td>
-                    {customerName}
-                </td>
-                {monthlyDataRows}
-            </tr>
-        )
+    ));
 
-    })
-    if(loading == false) {
-        return (
-            <table>
-                 <thead>
-                    <tr>
-                        <th>Customer Name</th>
-                        {monthHeaders}
-                    </tr>
-                    {tableDataRows}
-                </thead>
-               
-            </table>
-        ); 
-    } else {
-        return (
-            <div>Loading...</div>
-        )
-    }
+    return (
+      <tr>
+        <td>
+          {customerName}
+        </td>
+        {monthlyDataRows}
+      </tr>
+    );
+  });
+  if (loading == false) {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Customer Name</th>
+            {monthHeaders}
+          </tr>
+          {tableDataRows}
+        </thead>
+
+      </table>
+    );
+  }
+  return (
+    <div>Loading...</div>
+  );
 }
 
-export default MonthTables; 
+export default MonthTables;
