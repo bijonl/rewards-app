a07d051f1223f11d035f831c370121aa7124ac12
bijonl <bijon2591@gmail.com>
2023-11-13 11:54:08 -0500

    adding new data and adding total column

--- src/components/RewardTable.js
+++ src/components/RewardTable.js
@@ -1,5 +1,6 @@
 import React, { useMemo } from 'react'
 import PropTypes from 'prop-types'
+
 function calculateRewardPoints (dollars) {
   let points = 0
   if (dollars >= 50) {
@@ -13,21 +14,21 @@ function calculateRewardPoints (dollars) {
   return points
 }
 
-function getPointsPerMonthByID (data, date, ID) {
+function getPointsPerMonthByUsername (data, date, username) {
   const filteredTransactionsByMonth = data.filter((transaction) => {
     return date.toLocaleString('default', { month: 'long', year: 'numeric' }) === new Date(transaction.transactionDate).toLocaleString('default', { month: 'long', year: 'numeric' })
   })
 
-  const filteredMonthlyTransactionsByID = filteredTransactionsByMonth.filter((transaction) => {
-    return transaction.customerID === ID
+  const filteredMonthlyTransactionsByUsername = filteredTransactionsByMonth.filter((transaction) => {
+    return transaction.username === username
   })
 
-  const transactionPointAmountsMonthlyByID = filteredMonthlyTransactionsByID.map((transaction) => {
+  const transactionPointAmountsMonthlyByUsername = filteredMonthlyTransactionsByUsername.map((transaction) => {
     const transactionAmtDollars = Math.floor(transaction.transactionAmount.replace(/[^0-9.-]+/g, ''))
     return calculateRewardPoints(transactionAmtDollars)
   })
 
-  const totalPoints = transactionPointAmountsMonthlyByID.reduce(
+  const totalPoints = transactionPointAmountsMonthlyByUsername.reduce(
     (accumulator, currentValue) => accumulator + currentValue,
     0
   )
@@ -37,17 +38,8 @@ function getPointsPerMonthByID (data, date, ID) {
 
 function MonthTables ({ monthsToDisplay, allTransactionData, loading }) {
   // Loop though the months to create the tables. The array has store the timestamps of the days.
-  function getCustomerNameByID (transactionData, id) {
-    let customerName = ''
-    const transactionsWithID = transactionData.filter((transaction) => transaction.customerID === id)
-    transactionsWithID.forEach((customer) => {
-      customerName = customer.name
-    })
-    return customerName
-  }
-
   const uniqueCustomerIDs = useMemo(
-    () => ([...new Set(allTransactionData.map((element) => element.customerID))]), [allTransactionData]
+    () => ([...new Set(allTransactionData.map((element) => element.username))]), [allTransactionData]
   )
 
   if(loading) return <div>Loading...</div>
@@ -57,27 +49,38 @@ function MonthTables ({ monthsToDisplay, allTransactionData, loading }) {
             <table>
                  <thead>
                     <tr>
-                        <th>Customer Name</th>
+                        <th>Customer Username</th>
                         {monthsToDisplay.map((date, i) => {
                               return (
                                 <th key={i}>{date.toLocaleString('default', { month: 'long', year: 'numeric' })}</th>
                           )
                         })}
+                        <th>Total Points</th>
                     </tr>
                   </thead>
                   <tbody>
-                  {uniqueCustomerIDs.map((customerID, i) => (
-                        <tr key={i}>
-                            <td>
-                              {getCustomerNameByID(allTransactionData, customerID)}
-                            </td>
-                            {monthsToDisplay.map((date, i) => {
-                              return (
-                                <td key={i}>{getPointsPerMonthByID(allTransactionData, date, customerID)}</td>
-                              )
-                            })}
-                        </tr>
+                  {uniqueCustomerIDs.map((customerID, i) => {
+                      let userTotal = 0; 
+                      return (
+                        <>
+                         
+                          <tr key={i}>
+                              <td>
+                                {customerID}
+                              </td>
+                              {monthsToDisplay.map((date, i) => {
+                                userTotal += getPointsPerMonthByUsername(allTransactionData, date, customerID); 
+                                return (
+                                  <>
+                                  <td key={i}>{getPointsPerMonthByUsername(allTransactionData, date, customerID)}</td>
+                                  </>
+                                )
+                              })}
+                              <td>{userTotal}</td>
+                          </tr>
+                        </>
                       )
+                    }
                   )}
                   </tbody>
             </table>
