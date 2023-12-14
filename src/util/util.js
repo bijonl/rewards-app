d0967a22e74f35cc44cf1ce4f167bf66f0545e67
bijonl <bijon2591@gmail.com>
2023-12-14 18:27:10 -0500

    putting username check in a variable

--- src/util/util.js
+++ src/util/util.js
@@ -22,12 +22,8 @@ function getUniqueCustomerDataObject (transactionData) {
     // Makes the date month_years (ie november_2023)
     // This makes for an easy lookup on output later
     const formattedDateKey = formatDateToMonthYear(new Date(transaction.transactionDate)).replace(' ', '_').toLowerCase()
-    if (uniqueCustomerDataObject[transaction.username] === undefined) { // Check if username is NOT in the object
-      // Create new key-value pair with username as the key and empty object as value
-      uniqueCustomerDataObject[transaction.username] = {}
-      // Set username object key-value to the month_year and transaction amount in points.
-      uniqueCustomerDataObject[transaction.username][formattedDateKey] = calculateRewardPoints(transaction.transactionAmount)
-    } else { // Go into else block is username is already in the object.
+    const usernameInCustomerObject = uniqueCustomerDataObject[transaction.username] !== undefined
+    if (usernameInCustomerObject) { // Check if username is NOT in the object
       // Check if the month_year has been accounted for previously
       if (uniqueCustomerDataObject[transaction.username][formattedDateKey] !== undefined) {
         // If the date is already attached to that username, add the transaction amount in points.
@@ -36,6 +32,11 @@ function getUniqueCustomerDataObject (transactionData) {
         // If date is attached to the username, initialize that key-value paid in the transacation data object.
         uniqueCustomerDataObject[transaction.username][formattedDateKey] = calculateRewardPoints(transaction.transactionAmount)
       }
+    } else { // If username is NOT in the object
+      // Create new key-value pair with username as the key and empty object as value
+      uniqueCustomerDataObject[transaction.username] = {}
+      // Set username object key-value to the month_year and transaction amount in points.
+      uniqueCustomerDataObject[transaction.username][formattedDateKey] = calculateRewardPoints(transaction.transactionAmount)
     }
   })
   return uniqueCustomerDataObject
