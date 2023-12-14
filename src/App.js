30fe34c377b9ea54a5ee08140d6378656a23d2c8
bijonl <bijon2591@gmail.com>
2023-12-14 16:11:33 -0500

    adding unique custom logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo } from 'react'
 import RewardTable from './components/RewardTable.js'
-import { formatDateToMonthYear } from './util/util.js'
+import { formatDateToMonthYear, calculateRewardPoints } from './util/util.js'
 import './App.css'
 
 const API_URL = 'http://localhost:3500/transactions'
@@ -30,6 +30,31 @@ function App () {
   }, [])
 
   const monthYearsToDisplay = useMemo(() => getMonthYearsToDisplay(NUMBER_OF_MONTHS_TO_DISPLAY), [])
+
+  const uniqueCustomerData = Object.create(null);
+
+  console.log(allTransactionData); 
+  allTransactionData.map((transaction) => {
+    const date = formatDateToMonthYear(new Date(transaction.transactionDate)); 
+    const formattedDateKey = date.replace(' ', '_').toLowerCase(); 
+
+    if(uniqueCustomerData[transaction.username] === undefined) {
+      // Customer is not there
+      console.log('CUSTOMER NOT HERE'); 
+      uniqueCustomerData[transaction.username] = {}; 
+      uniqueCustomerData[transaction.username][formattedDateKey] = calculateRewardPoints(transaction.transactionAmount);
+      console.log('------');
+    } else {
+      console.log('CUSTOMER IS THERE'); 
+      if(uniqueCustomerData[transaction.username][formattedDateKey] !== undefined) {
+        uniqueCustomerData[transaction.username][formattedDateKey] += calculateRewardPoints(transaction.transactionAmount);
+      } else {
+        uniqueCustomerData[transaction.username][formattedDateKey] = calculateRewardPoints(transaction.transactionAmount);
+      }
+  }
+}); 
+
+  console.log(uniqueCustomerData); 
 
   return (
     <div className="App">
